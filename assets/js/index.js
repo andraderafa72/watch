560b227666fd1903ae3566211a808f8b6498f3dd
Rafael Andrade <devrafael72@gmail.com>
2021-06-19 21:07:07 -0300

    Ajustes de CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-const transformSize = 58;
+const transformSize = 59;
 
 setInterval(() => {
   const unitSeconds = document.querySelectorAll('.seconds .unidade span')
@@ -7,7 +7,7 @@ setInterval(() => {
   const dezenMinutes = document.querySelectorAll('.minutes .dezena span')
   const unitHours = document.querySelectorAll('.hours .unidade span')
   const dezenHours = document.querySelectorAll('.hours .dezena span')
-  
+
   const divUnitSeconds = document.querySelector('.seconds .unidade')
   const divDezenSeconds = document.querySelector('.seconds .dezena')
   const divUnitMinutes = document.querySelector('.minutes .unidade')
@@ -28,76 +28,76 @@ setInterval(() => {
   // SEGUNDOS
   for (let i = 0; i < 10; i++) {
     if(seconds[1] === `${i}`){
-      
+
       unitSeconds.forEach((el, index) => {
         el.classList.remove('selected')
       })
-      
+
       unitSeconds[i].classList.add('selected')
       console.log(divUnitSeconds)
       divUnitSeconds.style.transform = `translate3d(0, -${transformSize * i}px, 0)`;
-    }    
+    }
   }
 
   for (let i = 0; i < 5; i++) {
     if(seconds[0] === `${i}`){
-      
+
       dezenSeconds.forEach((el, index) => {
         el.classList.remove('selected')
       })
-      
+
       dezenSeconds[i].classList.add('selected')
       divDezenSeconds.style.transform = `translate3d(0, -${transformSize * i}px, 0)`;
-    }    
+    }
   }
 
   // MINUTOS
   for (let i = 0; i < 10; i++) {
     if(minutes[1] === `${i}`){
-      
+
       unitMinutes.forEach((el, index) => {
         el.classList.remove('selected')
       })
-      
+
       unitMinutes[i].classList.add('selected')
       divUnitMinutes.style.transform = `translate3d(0, -${transformSize * i}px, 0)`;
-    }    
+    }
   }
   for (let i = 0; i < 10; i++) {
     if(minutes[0] === `${i}`){
-      
+
       dezenMinutes.forEach((el, index) => {
         el.classList.remove('selected')
       })
-      
+
       dezenMinutes[i].classList.add('selected')
       divDezenMinutes.style.transform = `translate3d(0, -${transformSize * i}px, 0)`;
-    }    
+    }
   }
 
   // HORAS
   for (let i = 0; i < 10; i++) {
     if(hours[1] === `${i}`){
-      
+
       unitHours.forEach((el, index) => {
         el.classList.remove('selected')
       })
-      
+
       unitHours[i].classList.add('selected')
       divUnitHours.style.transform = `translate3d(0, -${transformSize * i}px, 0)`;
-    }    
+    }
   }
   for (let i = 0; i < 10; i++) {
     if(hours[0] === `${i}`){
-      
+
       dezenHours.forEach((el, index) => {
         el.classList.remove('selected')
       })
-      
+
       dezenHours[i].classList.add('selected')
       divDezenHours.style.transform = `translate3d(0, -${transformSize * i}px, 0)`;
-    }    
+    }
   }
 
- 
+
 }, 1000)
